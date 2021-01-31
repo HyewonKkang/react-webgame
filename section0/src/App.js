790834faf5f8fc18e4d2238d7b101c1edded3a71
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2021-02-01 04:18:25 +0900

    Add section0 Gugudan with react - 21/02/01

---
+++ section0/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Gugudan from './component/GuGuDan-class.js';
+import GuGuDan_hooks from './component/GuGuDan.js'
+function App() {
+  return (
+    <div className="App">
+      <Gugudan />
+      <br />
+      <GuGuDan_hooks />
+    </div>
+  );
+}
+
+export default App;
