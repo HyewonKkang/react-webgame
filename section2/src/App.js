0083e29c0b293d74e0a5814409dd7c11ee7310d3
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2021-02-05 00:00:55 +0900

    Add section 2 NumberBaseball with react - 21/02/05

---
+++ section2/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NumberBaseball from './components/NumberBaseball';
+import NumberBaseballClass from './components/NumberBaseballClass';
+
+function App() {
+  return (
+    <div className="App">
+      <NumberBaseballClass />
+      <NumberBaseball />
+    </div>
+  );
+}
+
+export default App;
