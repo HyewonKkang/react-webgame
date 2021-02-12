d3b9c1eecbbc16514e08699ee1e293896e10fc82
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2021-02-12 11:20:34 +0900

    Add section3 ResponseCheck - 21/02/12

---
+++ section3/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ResponseCheckClass from './components/ResponseCheckClass';
+import ResponseCheck from './components/ResponseCheck';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ResponseCheckClass /> */}
+      <ResponseCheck />
+    </div>
+  );
+}
+
+export default App;
