db325d21eb487c66cbdfe4e571f276de79840ff1
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2021-02-13 16:11:54 +0900

    Add section5 LottoClass - 21/02/13

---
+++ section5/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LottoClass from './components/LottoClass';
+
+function App() {
+  return (
+    <div className="App">
+      <LottoClass />
+    </div>
+  );
+}
+
+export default App;
