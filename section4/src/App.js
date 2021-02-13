d80fe787435c98d2dfe8359e42634b2ecadd2118
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2021-02-13 14:50:21 +0900

    Add section4 RSP - 21/02/13

---
+++ section4/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import RSPClass from './components/RSPClass';
+import RSP from './components/RSP';
+function App() {
+  return (
+    <div className="App">
+      {/* <RSPClass /> */}
+      <RSP />
+    </div>
+  );
+}
+
+export default App;
